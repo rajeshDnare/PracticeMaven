ace8305776073a2c5e134a4e9a648f059980f1cb
sujata_chavan <sujatachavan108@gmail.com>
2022-05-10 14:32:48 +0530

    commit from sujata

--- src/test/java/RajeshNare/PracticeMaven/Demo01.java
+++ src/test/java/RajeshNare/PracticeMaven/Demo01.java
@@ -5,9 +5,7 @@ import org.testng.annotations.Test;
 public class Demo01 {
 	@Test
 	public void product() {
-		System.out.println("This is just practice");
-		System.out.println("finaly it is getting done");
-		System.out.println("yes success");
+		
 		System.out.println("new branch created dev");
 		System.out.println("ready to mearge");
 		System.out.println("created develop branch");
